3182061d0a04c6d4207d034bae5e3db7adad1ecf
Semen <senia-2010@mail.ru>
2022-05-02 20:07:40 +0300

    upd swiper

--- app/js/sliderCompany.js
+++ app/js/sliderCompany.js
@@ -1,9 +1,3 @@
-let priceList = document.querySelector('.price__list');
-
-document.querySelector('.price__text').onclick = function (event) {
-  priceList.classList.toggle('price__list--hight');
-};
-
 new Swiper('.advantegs__column', {
 
   slidesPerView: 6,
@@ -27,4 +21,48 @@ new Swiper('.advantegs__column', {
       },
     }
   }
+});
+
+
+let priceWrapper = document.querySelector('.price__wrapper');
+let priceItems = document.querySelectorAll('.price__item'); 
+let priceList = document.querySelector('.price__list');
+
+
+
+window.addEventListener('resize', function() {
+  if(document.body.clientWidth > 1130) {
+    priceWrapper.classList.remove('swiper');
+    priceList.classList.remove('swiper-wrapper');
+    priceItems.forEach(item => item.classList.remove('swiper-slide'));
+  } else {
+    priceWrapper.classList.add('swiper');
+    priceList.classList.add('swiper-wrapper');
+    priceItems.forEach(item => item.classList.add('swiper-slide'));
+  }
+});
+
+
+
+document.querySelector('.price__text').onclick = function (event) {
+  priceList.classList.toggle('price__list--hight');
+};
+
+new Swiper('.price__wrapper', {
+
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 10,
+
+  pagination: {
+    el: '.price-pagination',
+  },
+ 
+
+  navigation: {
+    nextEl: '.price__nav-button--right',
+    prevEl: '.price__nav-button--left',
+  },
+
+  
 });
