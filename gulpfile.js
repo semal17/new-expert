2a68788daeb53f52ffb8258af46e068cc9bfdb22
Semen <senia-2010@mail.ru>
2022-05-12 14:26:59 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ function images() {
 
 function scripts() {
     return src([
-        'node_modules/jquery/dist/jquery.js',
+        // 'node_modules/jquery/dist/jquery.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
